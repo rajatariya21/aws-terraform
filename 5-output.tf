8b6676e10a90ee9b5be1edd00779ebf581bcc16b
Rajesh Jatav <rajatariya85@gmail.com>
2023-05-24 16:22:05 +0530

    create elastic ip to ec2 instance

--- 5-output.tf
+++ 5-output.tf
@@ -2,3 +2,8 @@
 output "ec2_global_ips" {
   value = ["${aws_instance.web.*.public_ip}"]
 }
+
+# Output of elastic IP address of ec2 instance
+output "ec2_elastic_ips" {
+  value = zipmap(aws_instance.web.*.tags.Name, aws_eip.demo-eip.*.public_ip)
+}
---
+++ 6-eip.tf
@@ -0,0 +1,17 @@
+# Create an EIP
+# # resource block for eip #
+# resource "aws_eip" "demo-eip" {
+#   vpc = true
+# }
+
+#  EIP to EC2 Instance
+resource "aws_eip" "demo-eip" {
+  instance = aws_instance.web.id
+  vpc      = true
+  }
+
+# #Associate EIP with EC2 Instance
+# resource "aws_eip_association" "demo-eip-association" {
+#   instance_id   = aws_instance.web.id
+#   allocation_id = aws_eip.demo-eip.id
+# }
